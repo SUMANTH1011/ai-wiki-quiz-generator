4b77a5cb14c52c2a6e56b7a7e9c90dbbdf227f9a
Sumanth Reddy <sumanth3296@gmail.com>
2026-01-28 02:19:25 +0530

    Added Backend URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "https://ai-wiki-quiz-generator-sage.vercel.app",
+        "https://ai-wiki-quiz-generator.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
